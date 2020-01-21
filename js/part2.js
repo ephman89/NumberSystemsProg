80214f32bddf33e6d227820529c1a2a304e05e43
JP Wong <jpwong7500@gmail.com>
2020-01-21 09:59:47 -0800

    Update part2.js
    attempting part 2

--- js/part2.js
+++ js/part2.js
@@ -1,11 +1,31 @@
 function ConversionPart2() {
     //
     var SignedDecimalInt = parseInt(document.getElementById("2_SignedInt").value);
+    var c = Math.abs(parseInt(document.getElementById("2_SignedInt").value));
+
+    var out = "";
+    while (c != 0)
+    {
+      out = c % 2 + out;
+      c = Math.trunc(c / 2);
+    }
+    while (out.length<32){
+      out = "0"+ out;
+    }
+
+    var outputValueTwosComplement = "";
+
+    for (var i =0; i< outputValue.length; i++){
+      if (out.charAt(i)==0){
+        outputValueTwosComplement += 1;
+      }
+      else {
+        outputValueTwosComplement += 0;
+      }
+    }
 
 
 
-    var outputValue = "010110101000110110011101";
-    var outputValueTwosComplement = "101001010111001001100011";
 
     // Show the output on the screen
     FormatAndShowOutput([outputValue, outputValueTwosComplement, SignedDecimalInt], 2);
